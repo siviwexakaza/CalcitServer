db92d67a0f00580fdbffef3562fc9c2903a16a3a
siviwe <xboyfan01>
2019-09-20 00:23:53 -0700

    Fix invoice

--- models/Invoice.js
+++ models/Invoice.js
@@ -3,7 +3,9 @@ const InvoiceSchema = mongoose.Schema({
     ContactID:{type:String},
     BusinessID:{type:String},
     Amount:{type:Number},
-    Date:{type:Date, default: Date.now}
+    Date:{type:Date, default: Date.now},
+    Due: {type:String},
+    Paid:{type:String}
 });
 
 module.exports = mongoose.model('Invoice',InvoiceSchema);
